a26a123b7ee30aa238c7378d509351b36d87f3ef
Zo <161946731+zozo0329@users.noreply.github.com>
2025-08-12 16:27:25 +0800

    added button

--- src/Components/Main/Item1/Item1.jsx
+++ src/Components/Main/Item1/Item1.jsx
@@ -3,9 +3,20 @@ import Item1Style from "./Item1.module.css";
 import item from "../../../Images/shoes.webp";
 const Item1 = () => {
   return (
-    <div className={Item1Style.item1}>
-      <img src={item} alt="item 1" />
-    </div>
+    <>
+      <div className={Item1Style.item1}>
+        <img src={item} alt="item 1" />
+      </div>
+      <div className={Item1Style.tags}>
+        <div className={Item1Style.title}>
+          <p>Nike Running</p>
+          <h1>More Choice. More Running.</h1>
+        </div>
+        <div className={Item1Style.btn}>
+          <button>Shop</button>
+        </div>
+      </div>
+    </>
   );
 };
 
